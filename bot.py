cbf8ecf105ae5666c53dccc822edddf51580f6dc
VasRonAr1 <rostislav.vasilchukovv@gmail.com>
2025-07-20 23:55:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ BOT_TOKEN = '7531365053:AAGVxAli7VlTyzySFDko5SKBw75VqpFtUyk'
 DATA_FILE = 'registered_chats.json'
 
 # Список разрешённых @username в Телеграм
-ALLOWED_USERNAMES = {'Kekdkddkfk', 'SpammBotss', 'Ujuvd', 'evenyqlp'}
+ALLOWED_USERNAMES = {'Kekdkddkfk', 'SpammBotsss', 'Ujuvd', 'evenyqlp'}
 
 # Список заблокированных user_id (добавь user_id пользователя, которого нужно заблокировать)
 BLOCKED_USER_IDS = {677387477}  # Замени на реальный user_id, если отличается
